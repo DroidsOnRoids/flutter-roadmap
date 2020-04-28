1cb7a60c09155cf8d05efc64db12b5172b957ab3
Damian Włodarczyk <damian.wlodarczyk.dod@gmail.com>
2020-04-28 10:57:16 +0200

    Create medium difficulty exercises and solutions

---
+++ dart/future/exercises/04/main.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+
+Future<void> main() async {
+  print('Loading message...');
+
+  final message = await _fetchMessage();
+  print(message);
+}
+
+Future<String> _fetchMessageFirstPart() =>
+    Future.delayed(
+      Duration(seconds: 2),
+          () => "Dart",
+    );
+
+Future<String> _fetchMessageSecondPart() =>
+    Future.delayed(
+      Duration(seconds: 1),
+          () => "is",
+    );
+
+Future<String> _fetchMessageThirdPart() =>
+    Future.delayed(
+      Duration(seconds: 3),
+          () => "easy!",
+    );
